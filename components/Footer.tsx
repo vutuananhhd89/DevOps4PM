23f20bc4603e64649545ba636e0a5f9f174fd413
Anh Vu <vutuananh.hd89@gmail.com>
2024-11-11 07:04:46 +0700

    about page updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,9 +12,9 @@ export default function Footer() {
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
-          {/* <SocialIcon kind="x" href={siteMetadata.x} size={6} />
-          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
+          {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} /> */}
+          <SocialIcon kind="x" href={siteMetadata.x} size={6} />
+          {/* <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
           <SocialIcon kind="medium" href={siteMetadata.medium} size={6} /> */}
         </div>
